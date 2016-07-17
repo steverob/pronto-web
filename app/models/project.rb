cdea77ed428c69f88021dbfe07dd8ef9b3d11e79
Steve Robinson <steve.rob@me.com>
2016-07-17 22:55:36 +0530

    delegating access_token from project to user

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
+
+  def access_token
+    user.access_token_for scm_service: self.scm_service
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :projects
+
+  def access_token_for scm_service:
+    send "#{scm_service}_access_token"
+  end
 end
