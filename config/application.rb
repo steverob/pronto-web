a5e24b582026344b3dae2dabb3bdee75fd94307f
Steve Robinson <steve.rob@me.com>
2016-07-17 22:57:24 +0530

    configured ActiveJob backend as sidekiq

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module ProntoWeb
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
+    config.active_job.queue_adapter = :sidekiq
     config.generators do |g|
       g.test_framework  false
       g.stylesheets     false
