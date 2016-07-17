850a849daad466c1f421a3ec9ae6ba134bf4d2e6
Steve Robinson <steve.rob@me.com>
2016-07-17 22:56:58 +0530

    keeping generator outputs clean for now

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,11 @@ Bundler.require(*Rails.groups)
 module ProntoWeb
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helpers         false
+    end
   end
 end
