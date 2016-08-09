f4cd0a068c0aff915820cdbd9b8708e6a6cf92a9
Steve Robinson <steve.rob@me.com>
2016-08-09 23:33:26 +0530

    added Reviewer class inside ProntoWeb and calling it from PRReview model

--- app/models/pull_request_review.rb
+++ app/models/pull_request_review.rb
@@ -2,7 +2,7 @@ class PullRequestReview < ApplicationRecord
   belongs_to :project
   delegate :scm_service, to: :project
 
-  def review    
-    ProntoWeb.review_pull_request self
+  def review
+    ProntoWeb.new(self).send "review_#{scm_service}_pull_request"
   end
 end
---
+++ lib/pronto_web/reviewer.rb
@@ -0,0 +1,18 @@
+module ProntoWeb
+  class Reviewer  
+    def initialize pull_request_review
+      @pull_request_review = pull_request_review
+    end
+
+    def review_github_pull_request project:, pull_request_data:
+      Github::Reviewer.new(
+        project: @pull_request_review.project,
+        pull_request_data: Github::PullRequest.new(@pull_request_review.payload)
+      ).review
+    end
+
+    def self.review_gitlab_merge_request
+      raise NotImplementedError
+    end
+  end
+end
