2fe6b332d41f169f0ed1b4dd3d5b81e50a249f3a
Steve Robinson <steve.rob@me.com>
2016-08-21 16:52:53 +0530

    handling PR Webhooks only if action is opened / synchronized

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,11 +2,16 @@ class WebhooksController < ApplicationController
   protect_from_forgery with: :null_session
   
   def github_pr
-    github_project.create_pr_review(params.permit!)
+    github_project.create_pr_review(params.permit!) if handle_pr_action?
     head :ok
   end
 
   private
+
+  def handle_pr_action?
+    params["action"] == "opened" ||
+    params["action"] == "synchronize"
+  end
 
   def github_project
     Project.find_by external_id: params["repository"]["id"]
